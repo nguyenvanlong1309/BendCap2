2e552c48c3cf00b57ead788573dab199086f50fa
nguyenvanlong1309 <108638225+nguyenvanlong1309@users.noreply.github.com>
2023-03-01 21:19:07 +0700

    Upload

---
+++ src/routes/site.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const siteController = require('../app/controller/SiteController');
+
+// siteController.index
+
+router.use('/search', siteController.search);
+router.use('/', siteController.index);
+
+module.exports = router;
